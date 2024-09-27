45a85f2ab1678ca9908777b33173eba03e0ac721
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-09-27 13:02:11 +0900

    add tests

--- Tests/FunctionCalling-MacPaw-OpenAITests/FunctionCalling_MacPaw_OpenAITests.swift
+++ Tests/FunctionCalling-MacPaw-OpenAITests/FunctionCalling_MacPaw_OpenAITests.swift
@@ -1,6 +1,37 @@
 import Testing
 @testable import FunctionCalling_MacPaw_OpenAI
+import FunctionCalling
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+actor MacPawOpenAITests {
+    @FunctionCalling(service: .claude)
+    struct FunctionContainer {
+        /// Return current weather of location that passed by the argument
+        /// - Parameter location: location that
+        /// - Returns: string of weather
+        @CallableFunction
+        func getWeather(location: String) -> String {
+            return "Sunny"
+        }
+        
+        @CallableFunction
+        func getStock(args: String) -> Int {
+            return 0
+        }
+    }
+
+    @Test func checkConvertedResults() async throws {
+        let functions = FunctionContainer().macPawOpenAITools
+        
+        #expect(functions.count == 2)
+        
+        let getWeather = try #require(functions.first?.function)
+        #expect(getWeather.name == "getWeather")
+        #expect(getWeather.description == """
+        Return current weather of location that passed by the argument- Parameter location: location that- Returns: string of weather
+        """)
+        
+        let getStock = try #require(functions[1].function)
+        #expect(getStock.name == "getStock")
+        #expect(getStock.description == "")
+    }
 }
